96b2fc01f0bc38cd5f4b2b130446532d5e8fe675
Mr-OctoSquid <87108267+Mr-OctoSquid@users.noreply.github.com>
2023-06-13 10:04:18 -0700

    Create STFP Sync Tool.bat

---
+++ STFP Sync Tool.bat
@@ -0,0 +1,17 @@
+@echo off
+rem "Changes operating directory to directory of winscp.com"
+cd "C:\Program Files (x86)\WinSCP"
+
+rem "Writes start of script time to log file, log file location may need to manually be changed."
+echo ----Script Start----- >> "C:\Helixstorm SFTP Sync tool\log.txt"
+echo %date% %time% >> "C:\Helixstorm SFTP Sync tool\log.txt"
+echo --------------------- >> "C:\Helixstorm SFTP Sync tool\log.txt"
+
+rem "Calls winscp.com, sets log file for application and passes script for application. Waits for winscp.com to compl
+ete."
+WinSCP.com /log=>>"C:\Helixstorm SFTP Sync tool\log.txt" /script="C:\Helixstorm SFTP Sync tool\script.txt" /wait
+
+rem "Writes end of script time to log file, log file location may need to manually be changed."
+echo ----Script End----- >> "C:\Helixstorm SFTP Sync tool\log.txt"
+echo %date% %time% >> "C:\Helixstorm SFTP Sync tool\log.txt"
+echo ------------------- >> "C:\Helixstorm SFTP Sync tool\log.txt"
